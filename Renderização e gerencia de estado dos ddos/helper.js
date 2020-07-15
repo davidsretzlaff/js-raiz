43b6af6d752d0554bde1a4afc26b8eaf1264528c
David Retzlaff <davids.retzlaff@gmail.com>
2020-07-15 20:27:05 -0300

    Examples FILTER and FILTER + MAP

--- Renderização e gerencia de estado dos ddos/helper.js
+++ Renderização e gerencia de estado dos ddos/helper.js
@@ -29,7 +29,28 @@ const people = [
     name:'outro'
 }
 ];
+// test map for me understand better
 console.log('test map',people.map(i => i.name));
+const testMap2 = people.map(i => {
+    if(i.name === "david")
+        return true;
+});
+console.log(testMap2);
+// SUMMARY OF THE MAP IS THAT IT WILL GO THROUGH THE WHOLE ARRAY AND INSERT FUNCTION IN EACH INDEX.
+// EACH INDEX WILL HAVE A RETURN.
 
-// EXAMPLE FILTER
-let mult2 = [];
+
+
+// ------ EXAMPLE FILTER -------
+
+console.log(people.filter(i =>{ i.name === "david"}));
+
+//SUMMARY OF THE FILTER, IT WILL RETURN ONLY WHAT TO PASS IN THE FUNCTION VALIDATION 
+//USING FOR FILTER ARRAY AND FORMAT TO A NEW ARRAY
+
+// ----- EXAMPLE USING FILTER AND MAP ------
+
+const arrayMapFilter = people.filter(i => i.name === "david").map(i => i.name + ' Retzlaff');
+console.log("arrayMapFilter", arrayMapFilter);
+
+// USING FILTER FOR FIND PEOPLE WITH NAME DAVID AND APPLY LAST NAME WITH MAP
