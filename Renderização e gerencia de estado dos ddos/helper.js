67c8055506ba2aed1c124e1a831cdc1794912772
David Retzlaff <davids.retzlaff@gmail.com>
2020-07-15 21:16:29 -0300

    EXAMLE REDUCE FOR RETURN ARRAY OF ADULTS PEOPLES AND ANOTHER ARRAY OF MINORS PEOPLES

--- Renderização e gerencia de estado dos ddos/helper.js
+++ Renderização e gerencia de estado dos ddos/helper.js
@@ -23,11 +23,17 @@ console.log("namesLowercaseAndArrow",namesLowercaseAndArrow);
 // using map with array object
 const people = [
     {id: 1,
-    name : 'david' 
+    name : 'david' ,
+    old : 20
 },{
     id: 2,
-    name:'outro'
-}
+    name:'janice',
+    old : 15
+},
+{   id: 3,
+    name : 'bruna' ,
+    old : 22
+},
 ];
 // test map for me understand better
 console.log('test map',people.map(i => i.name));
@@ -49,8 +55,45 @@ console.log(people.filter(i =>{ i.name === "david"}));
 //USING FOR FILTER ARRAY AND FORMAT TO A NEW ARRAY
 
 // ----- EXAMPLE USING FILTER AND MAP ------
-
+// USING FILTER FOR FIND PEOPLE WITH NAME DAVID AND APPLY LAST NAME WITH MAP
 const arrayMapFilter = people.filter(i => i.name === "david").map(i => i.name + ' Retzlaff');
 console.log("arrayMapFilter", arrayMapFilter);
 
-// USING FILTER FOR FIND PEOPLE WITH NAME DAVID AND APPLY LAST NAME WITH MAP
+
+// ------- EXAMPLE REDUCE -------
+const arrayNumbersReduce = [1,2,3];
+
+// add up all the numbers in the array
+function addNumbers()
+{
+    let soma = 0;
+    for(let i = 0 ; i < arrayNumbersReduce.length; i++)
+    {
+        soma += arrayNumbersReduce[i];
+    }
+    return soma;
+};
+console.log("addNumbersWithFor",addNumbers());
+
+
+// instructions reduce
+// reduce 1 paramiter = function(accomodatorValue,arrayValue)
+// reduce 2 parameter = value initial of accomodatorValue 
+// add up all the numbers in the array using reduce
+
+// const addNumbersReduce = arrayNumbersReduce.reduce(function(accomodatorValue,arrayValue){
+//     return accomodatorValue + arrayValue;
+// },0);
+// using reduce with arrow function
+
+const addNumbersReduce = arrayNumbersReduce.reduce((accomodatorValue,arrayValue) => accomodatorValue + arrayValue,0);
+console.log('addNumbersReduce',addNumbersReduce);
+
+//using reduce return array of adults people  and another array of minors people 
+const peoplesFilter = people.reduce(function(acomodatorValue,arrayValue)
+{
+    const propAdultsMinors = arrayValue.old >= 18 ? 'adults' : 'minors';
+    acomodatorValue[propAdultsMinors].push(arrayValue);    
+    return acomodatorValue;
+},{ adults: [], minors: []})
+console.log('peoplesFilter',peoplesFilter);
